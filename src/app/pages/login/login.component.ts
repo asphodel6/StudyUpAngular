0c1f04118db9691218c69be02b5b64ccfb2912ea
asphodel6 <harmsss@mail.ru>
2023-05-06 17:35:26 +0500

    Added registration page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit{
 
--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
 
+  public registrationForm!: FormGroup;
+
+  public ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]),
+      'userName': new FormControl('', [Validators.required])
+    });
+  }
+
+  public createUser(): void {
+    alert('create');
+  }
+
+  public get email(): any {
+    return this.registrationForm.get('email');
+  }
+
+  public get password(): any {
+    return this.registrationForm.get('password');
+  }
+
+  public get userName(): any {
+    return this.registrationForm.get('userName');
+  }
 }
