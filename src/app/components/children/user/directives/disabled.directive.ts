10b6f747c62e6e8a35510c10eb2586af8196a64d
asphodel6 <harmsss@mail.ru>
2023-06-13 15:51:16 +0500

    Refactoring

--- src/app/components/children/user/directives/disabled.directive.ts
+++ src/app/components/children/user/directives/disabled.directive.ts
@@ -7,8 +7,6 @@ export class DisabledDirective implements OnChanges{
   @Input() public counts!: number | null;
   @Input() public position!: number;
   @HostBinding('style.pointer-events') private _pointerEvents!: string;
-  @HostBinding('style.backgroundColor') private _back!: string;
-  @HostBinding('style.background-image') private _src!: string;
 
   public ngOnChanges(): void {
     if (<number>this.counts !== this.position) {
