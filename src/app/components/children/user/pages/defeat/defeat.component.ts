162b2f907ce2db4fa9ab2b5bea30037f3b260d95
asphodel6 <harmsss@mail.ru>
2023-06-15 01:19:06 +0500

    Refactoring

--- src/app/components/children/user/pages/defeat/defeat.component.ts
+++ src/app/components/children/user/pages/defeat/defeat.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-defeat',
   templateUrl: './defeat.component.html',
-  styleUrls: ['./defeat.component.scss']
+  styleUrls: ['./defeat.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DefeatComponent {
+  constructor(private _router: Router) {
 
+  }
+
+  public navigateMain(): void {
+    this._router.navigate(['user']);
+  }
+
+  public navigateSearch(): void {
+    this._router.navigate(['user/search']);
+  }
 }
--- src/app/components/children/user/pages/victory/victory.component.ts
+++ src/app/components/children/user/pages/victory/victory.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-victory',
   templateUrl: './victory.component.html',
-  styleUrls: ['./victory.component.scss']
+  styleUrls: ['./victory.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VictoryComponent {
+  constructor(private _router: Router) {
 
+  }
+
+  public navigateMain(): void {
+    this._router.navigate(['user']);
+  }
+
+  public navigateSearch(): void {
+    this._router.navigate(['user/search']);
+  }
 }
