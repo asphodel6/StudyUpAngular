a9a19ee6e7d57680e6ddfb3a268aa5797c5f0d5c
asphodel6 <harmsss@mail.ru>
2023-07-09 22:26:28 +0500

    update game template

--- src/app/components/children/user/pages/battlefield/battlefield.component.ts
+++ src/app/components/children/user/pages/battlefield/battlefield.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnDestroy} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { heartsCount$ } from '../../service/game.service';
 import { rightAnswersCount$ } from '../../service/game.service';
 import { BehaviorSubject } from 'rxjs';
@@ -12,10 +12,4 @@ import { BehaviorSubject } from 'rxjs';
 export class BattlefieldComponent{
   public hearts$: BehaviorSubject<number> = heartsCount$;
   public answers$: BehaviorSubject<number> = rightAnswersCount$;
-
-  // public ngOnDestroy(): void {
-  //   // this.hearts$.next(3);
-  //   // this.answers$.next(0);
-  //   // console.log('server ymer');
-  // }
 }
